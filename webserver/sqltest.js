92dbb9b64cfae5ed30bee6289edd649eab856b58
Morgan-Middleton <mmidd001@gmail.com>
2022-01-31 16:18:51 +1300

    SQL queires partially parameterized

--- webserver/sqltest.js
+++ webserver/sqltest.js
@@ -1,10 +1,12 @@
 // https://docs.microsoft.com/en-us/azure/azure-sql/database/connect-query-nodejs?tabs=windows
 
-const { Connection, Request } = require("tedious");
+const { Connection, Request, TYPES } = require("tedious");
 const username = "Morgan"
 const password = "TestDB0001928"
 const server_name = "speakscents-test-server.database.windows.net"
 const db_name = "test_database"
+
+var trialJsonString = {"scent_suggestions":"asdfasdfasd","day_or_night":"day","season":"summer","gender":"masculine","moods":["classic","fresh"],"scent_styles":["woody","fruity","spicy"]}
 
 // Create connection to database
 const config = {
@@ -22,54 +24,70 @@ const config = {
   }
 };
 
-/* 
-    //Use Azure VM Managed Identity to connect to the SQL database
-    const config = {
-        server: process.env["db_server"],
-        authentication: {
-            type: 'azure-active-directory-msi-vm',
-        },
-        options: {
-            database: process.env["db_database"],
-            encrypt: true,
-            port: 1433
-        }
-    };
-
-    //Use Azure App Service Managed Identity to connect to the SQL database
-    const config = {
-        server: process.env["db_server"],
-        authentication: {
-            type: 'azure-active-directory-msi-app-service',
-        },
-        options: {
-            database: process.env["db_database"],
-            encrypt: true,
-            port: 1433
-        }
-    });
-
-*/
-
 const connection = new Connection(config);
 
-// Attempt to connect and execute queries if connection goes through
-connection.on("connect", err => {
-  if (err) {
-    console.log("error flag")
-    console.error(err.message);
-  } else {
-      console.log("successful connection")
-      //insertToDatabase();
-      queryFromDatabase();
+const runQuery = function(JSON){
+  console.log("I'm tyrin gto connect")
+  // Attempt to connect and execute queries if connection goes through
+    connection.on("connect", err => {
+      if (err) {
+        console.log("error flag")
+        console.error(err.message);
+      } else {
+          console.log("successful connection")
+          insertToDatabase(JSON);
+      }
+    });
   }
-});
+
+// function runQuery(JSON){
+// // Attempt to connect and execute queries if connection goes through
+//   connection.on("connect", err => {
+//     if (err) {
+//       console.log("error flag")
+//       console.error(err.message);
+//     } else {
+//         console.log("successful connection")
+//         insertToDatabase(JSON);
+//     }
+//   });
+// }
 
 connection.connect();
+runQuery("{\"scent_suggestions\":\"asdfasdfasd\",\"day_or_night\":\"day\",\"season\":\"summer\",\"gender\":\"masculine\",\"moods\":[\"classic\",\"fresh\"],\"scent_styles\":[\"woody\",\"fruity\",\"spicy\"]}");
 //insertToDatabase();
 //queryFromDatabase();
 
-function insertToDatabase() {
+function insertToDatabase(unparsedJSON){
+  console.log("Inserting into Table...");
+  var parsedJSON = JSON.parse(unparsedJSON);
+  var list = [];
+  for(data in parsedJSON){
+    list.push(data);
+  }
+  var q1 = list[0];
+  // var q2 = list[1];
+  // var q3 = list[2];
+  // var q4 = list[3];
+  // var q5 = list[4];
+  // var q6 = list[5];
+
+  var query = `INSERT INTO quiz_results (customer_id, answer_path, cluster, quiz_id, quiz_version, question_1, question_2, question_3, question_4, question_5, question_6) `;
+  var values = `VALUES ('123', 'answer', 'cluster', '123e4567-e89b-12d3-a456-426614174000', '1', '@q1', 'q2', 'q3', 'q4', 'q5', 'q6')`;
+  const request = new Request(query + values
+    , (err) => {
+      if (err) {
+        console.log("Unable to insert data")
+        console.error(err.message);
+      } else {
+        console.log("Data inserted");
+      }
+    });
+    request.addParameter('q1', TYPES.VarChar, q1);
+  connection.execSql(request);
+}
+
+function insertDummyIntoDatabase() {
   console.log("Inserting into Table...");
 
   // Insert dummy data
@@ -105,34 +123,8 @@ function queryFromDatabase() {
   console.log(connection.execSql(request));
 }
 
-// function queryDatabase() {
-//   console.log("Reading rows from the Table...");
-
-//   // Read all rows from table
-//   const request = new Request(
-//     `SELECT TOP 20 pc.Name as CategoryName,
-//                    p.name as ProductName
-//      FROM [SalesLT].[ProductCategory] pc
-//      JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid`,
-//     (err, rowCount) => {
-//       if (err) {
-//         console.error(err.message);
-//       } else {
-//         console.log(`${rowCount} row(s) returned`);
-//       }
-//     }
-//   );
-
-//   request.on("row", columns => {
-//     columns.forEach(column => {
-//       console.log("%s\t%s", column.metadata.colName, column.value);
-//     });
-//   });
-
-//   connection.execSql(request);
-// }
-
 function consoleTest(){
   console.log("hello123")
 }
 
+module.exports = {runQuery}
