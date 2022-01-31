ada72b788bd6fbddf7c959bea7cfefbb44c10572
charlotte565 <cralphs@hotmail.com>
2022-02-01 11:21:16 +1300

    Added styling to qiuz_results and navigation

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -4,6 +4,9 @@ var multer = require('multer')
 var upload = multer()
 var app = express()
 
+// var flash = require('connect-flash')
+// var ejs = require('ejs')
+
 // app.use(express.static("speakscents"));
 app.use('/css',express.static(__dirname +'/css'));
 
@@ -45,7 +48,8 @@ app.post('/quiz-submit', function(req,res){
 
     quiz.get_json(req.body)
 
-    res.send("Results Recieved")
+    // res.send("Results Recieved")
+    res.sendFile(__dirname + '/quiz_results.html')
 })
 
 //registration page
@@ -57,5 +61,37 @@ app.post('/registration-submit', function(req,res){
 
     reg.get_json(req.body)
     res.sendFile(__dirname + '/quiz.html')
+
+})
+
+//This function returns the saved results from the DB and presents it back to the user
+app.get('/quiz-results', function(req,res){
+
+    //Data from sql
+    var data ="abcdefghijklmenop"
+    
+    // let html = ejs.render('<%= people.join(", "); %>', {people: people});
+
+    var suggestions = "hellow"
+    var time = "mate"
+    var season = "what's going on?"
+    var scentStrength = "you ok buddy?"
+    var scentMood = "see ya matey"
+    var scentStyles = "potatey"
+
+
+    res.render("quiz_results", {
+        suggestions: suggestions,
+        time: time,
+        season: season,
+        scentStrength: scentStrength ,
+        scentMood: scentMood,
+        scentStyles: scentStyles
+    })
+
+
+
+
+
 
 })
