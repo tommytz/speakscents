c99f92e1656ba8da49ae92106a0be7bba532f0ee
adlebita <lebita.adrian@gmail.com>
2022-02-02 11:55:22 +1300

    Commenting for files and readMe file created.

--- webserver/README.txt
+++ webserver/README.txt
@@ -4,11 +4,11 @@ Backend Installation
 1. Install node js from https://nodejs.org/en/
 2. Check if installed by running in terminal 
     node -v
-3. In terminal install these dependence by injecting this line:
+3. In terminal install the required dependencies by injecting this line:
     node install express body-parser multer connect-flash tedious ejs
-   If you get an error, you may need to install npm, node install 
 4. Check to see you have the dependencies isntalled by typing in terminal, 
     npm list
 
 FrontEnd Installation
+Note: To view .ejs webpages must have the ejs module installed, see above.
 TBA
--- webserver/quiz.js
+++ webserver/quiz.js
@@ -1,5 +1,15 @@
 
-const { start } = require("repl");
+/* This module processes form data. 
+ * All data should  be validated in here
+ * This module does the following:
+ * - Recieves form data
+ * - Validates data
+ * - Returns an object of some form
+ * 
+ * ===========================================================
+ * All functions that recieve form data MUST return an object
+ * ===========================================================
+ */
 
 /* 
  *  This function retrieves the results from HTML form when submitted and returns a JSON object.
@@ -7,71 +17,83 @@ const { start } = require("repl");
  */
 const get_results = function (body) {
 
-  var quiz_data = "{";
+  var data = "{";
 
-  var quiz_elements = document.getElementById("quiz").elements;
+  var data_elements = document.getElementById("quiz").elements;
 
-  for (var i = 0; i < quiz_elements.length - 1; i++) {
+  for (var i = 0; i < data_elements.length - 1; i++) {
 
-    if (quiz_elements[i].type === "radio" || quiz_elements[i].type === "checkbox") {
-      quiz_data = quiz_data + `"${quiz_elements[i].getAttribute("id")}":"${quiz_elements[i].checked}"`;
+    if (data_elements[i].type === "radio" || data_elements[i].type === "checkbox") {
+      data = data + `"${data_elements[i].getAttribute("id")}":"${data_elements[i].checked}"`;
 
-    } else if (quiz_elements[i].type === "text") {
-      quiz_data = quiz_data + `"${quiz_elements[i].getAttribute("id")}":"${quiz_elements[i].value}"`;
+    } else if (data_elements[i].type === "text") {
+      data = data + `"${data_elements[i].getAttribute("id")}":"${data_elements[i].value}"`;
 
     }
 
-
-    if (i === quiz_elements.length - 2) { //The last question. The last element in the form will always be the submit button, hence -2.
-      quiz_data = quiz_data + "}";
+    if (i === data_elements.length - 2) { //The last question. The last element in the form will always be the submit button, hence -2.
+      data = data + "}";
     } else {
-      quiz_data = quiz_data + ",";
+      data = data + ",";
     }
   }
+  
+  //Creates JSON Object
+  var data_JSON = JSON.parse(data);
 
-  var quiz_JSON = JSON.parse(quiz_data);
-  console.log(quiz_JSON);
-
-  return quiz_JSON;
+  return data_JSON;
 };
 
 
-//This method returns string version of json file to parse for information, to be sent to the SQL db
+//
+//Returning straight JSON file of the form data.
+//
 const get_json = function (body) {
-
-
-  // var quiz_string = JSON.stringify(body);
-  // console.log(quiz_string);
-  // return quiz_string;
-  // // console.log(quiz_string)
-
-
-  // // this returns an array list
-  // // var results = []
-
-  // // for(var key in body){
-  // //     results.push([key, body[key]])
-  // // }
-
-  // // console.log(results)
-  // // console.log(results.length)
-
-  // // if(results[0].includes('night')){
-  // //     console.log(true)
-  // // } else {
-  // //     console.log(false)
-  // // }
-
-  // // another style depending on how sql gets populated with quiz results
-  // var results = []
-
-  // for(var key in body){
-  //     results.push(body[key])
-  // }
-
-  // console.log(results)
 
   return body
 };
 
-module.exports = { get_json };
+
+
+//
+//This method takes form data and returns an array list of [key,value]
+//
+const get_jsonAsList = function (body) {
+
+  var results = []
+
+  for(var key in body){
+      results.push([key, body[key]])
+  }
+
+
+  return results
+};
+
+//
+//This method takes form data and returns an array list of [values] only
+//
+const get_jsonAsListOfValues = function (body) {
+
+  var results = []
+  
+  for(var key in body){
+      results.push(body[key])
+  }
+
+  return results
+};
+
+
+//
+//This method takes form data and returns json string
+//
+const get_jsonAsString = function (body) {
+
+  var data = JSON.stringify(body);
+
+  return data;
+
+};
+
+module.exports = { get_json, get_jsonAsList, get_jsonAsListOfValues, get_jsonAsString, get_results };
