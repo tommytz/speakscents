5436b1fb5967b389b20656f33945a34609ab5378
adlebita <lebita.adrian@gmail.com>
2022-02-14 13:06:28 +1300

    Quiz results view popup first. Comments added

--- webserver/popup.js
+++ webserver/popup.js
@@ -3,12 +3,12 @@
  * It does not load images or content. Simply controls the display and events on the popup
  * */
 
+//Selection of html elements with querySelectorAll and overlay element.
 const openPopupBtn = document.querySelectorAll("[data-modal-target]");
 const closePopupBtn = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
 
-console.log("hello world!")
-
+//Adding event listener for each button that is set with [data-modal-button]
 openPopupBtn.forEach(button => {
     button.addEventListener("click", () => {
         const popup = document.querySelector(button.dataset.modalTarget);
@@ -16,6 +16,7 @@ openPopupBtn.forEach(button => {
     });
 });
 
+//Adding event listener for each button that is set with [data-close-button]
 closePopupBtn.forEach(button => {
     button.addEventListener("click", () => {
         const close = button.closest(".popup");
@@ -23,6 +24,7 @@ closePopupBtn.forEach(button => {
     });
 });
 
+//Adds a class "active" that triggers CSS styling in popup.css
 function openPopup(modal){
     if(modal == null){
         return;
@@ -31,6 +33,7 @@ function openPopup(modal){
     overlay.classList.add("active");
 }
 
+//Removes the class "active" that triggers CSS styling in popup.css
 function closePopup(modal){
     if(modal == null){
         return;
@@ -39,6 +42,8 @@ function closePopup(modal){
     overlay.classList.remove("active");
 }
 
+
+//Adding event listener for the overlay, if the user clicks outside while the popup is active, will exit popup.
 overlay.addEventListener("click", () => {
     const popups = document.querySelectorAll(".popup.active");
     popups.forEach(popups => {
